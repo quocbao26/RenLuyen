01747a60476ec34221feb5fe603521ffcbffc34e
QuocBao <quocbao16vn@gmail.com>
2019-01-13 14:47:45 +0700

    70 71

---
+++ vd1-Class/70.py
@@ -0,0 +1,6 @@
+"""
+Viết các lệnh assert để xác minh rằng tất cả các số trong list [2,4,6,8] là chẵn.
+"""
+lst = [2,4,6,8]
+for i in lst:
+    assert i%2==0
---
+++ vd1-Class/71.py
@@ -0,0 +1,10 @@
+"""
+Viết chương trình chấp nhận biểu thức toán học cơ bản do người dùng nhập vào từ bảng điều khiển
+và in kết quả ước lượng ra ngoài màn hình.
+Ví dụ: Nếu chuỗi sau là đầu vào của chương trình:
+35 + 3
+thì đầu ra sẽ lả:
+38
+"""
+s = eval(input("Nhập chuỗi tính: "))
+print(s)
